dc4fbd4f954193819e58eede104933e01d6f84bd
Abdul Aziz <abdulaziz758015@gmail.com>
2025-08-08 11:04:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Outlet } from 'react-router'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='max-w-screen-2xl mx-auto'>
+    
+      <Outlet></Outlet>
+      
+   </div>
+  )
+}
+
+export default App
