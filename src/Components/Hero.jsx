31180c1a9043b153ab8aa095f7ece1ccd179a66c
Abdul Aziz <abdulaziz758015@gmail.com>
2025-08-09 19:32:44 +0600

    created a search bar

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { IoSearchOutline } from "react-icons/io5";
+
+const Hero = () => {
+  return (
+    <div className="px-5 xl:px-10 md:w-1/2 mb-10">
+      <h1 className="mt-6 mb-10 text-4xl xl:text-6xl text-center font-bold text-[#2A3342] leading-normal xl:leading-relaxed">
+        Quick and Tasty Meal<span className="text-orange-300"> Ideas</span>{" "}
+      </h1>
+      <form
+        action="/search"
+        className="bg-white flex items-center p-4 rounded relative"
+      >
+        <IoSearchOutline className="w-5 h-5 text-neutral-300 mr-2" />
+        <input
+          className="outline-none w-full placeholder:text-[#1b2629]"
+          name="query"
+          type="search"
+          placeholder="search for a recipes"
+          id="search"
+          required=""
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from '../../Components/Hero';
 
 const Home = () => {
     return (
-        <div>
-            <p>Home</p>
+        <div className='container mx-auto'>
+           <div className='flex flex-col justify-center items-center py-20 w-full'>
+            <Hero></Hero>
+           </div>
         </div>
     );
 };
